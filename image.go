7579437afeec42cb259bba7e5e14bc5e084a13fa
heydabop <heydabop@gmail.com>
2014-04-17 14:49:49 -0500

    made image actually work

--- image.go
+++ image.go
@@ -4,17 +4,19 @@ import(
 	"code.google.com/p/go-tour/pic"
 	"image"
 	"image/color"
+	"time"
+	"math/rand"
 )
 
 type Pixel struct{
-	R uint32
-	G uint32
-	B uint32
-	A uint32
+	R uint8
+	G uint8
+	B uint8
+	A uint8
 }
 
-func (p Pixel) RGBA() (r, g, b, a uint32){
-	return p.R, p.G, p.B, p.A
+func (p Pixel) RGBA() color.Color{
+	return color.RGBA{p.R, p.G, p.B, p.A}
 }
 
 type Image struct{
@@ -32,14 +34,22 @@ func (i Image) Bounds() image.Rectangle{
 }
 
 func (i Image) At(x, y int) color.Color{
-	return i.Img [x][y]
+	return i.Img[x][y].RGBA()
 }
 
 func main(){
+	rand.Seed(time.Now().Unix())
+
 	x, y := 100, 100
 	img := make([][]Pixel, x)
 	for i := 0; i < x; i++ {
 		img[i] = make([]Pixel, y)
+		for j := 0; j < y; j++ {
+			img[i][j] = Pixel{uint8(rand.Intn(255)),
+				uint8(rand.Intn(255)),
+				uint8(rand.Intn(255)),
+				255}
+		}
 	}
 	m := Image{x, y, img}
 	pic.ShowImage(m)
